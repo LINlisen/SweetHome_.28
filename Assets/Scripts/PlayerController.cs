b7a2bef314c3dd05b802653b838bcd9d9c6becaf
LINlisen <z1x2c3v441@gmail.com>
2021-08-21 23:54:59 +0800

    candy direction update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -102,7 +102,7 @@ public class PlayerController : MonoBehaviour
         _bIsDash = true;
         playerAni.SetBool("Dash", true);
         directionXOZ.y = 0f;// 只做平面的上下移动和水平移动，不做高度上的上下移动
-        directionXOZ = playerController.transform.right;// forward 指向物体当前的前方
+        directionXOZ = -playerController.transform.right;// forward 指向物体当前的前方
         
         GameObject.Find(playerName).gameObject.transform.GetChild(2).gameObject.SetActive(true);
        
