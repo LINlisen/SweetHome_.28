910aab3226672e73997766594e4c26a274b0fa60
LINlisen <z1x2c3v441@gmail.com>
2021-09-01 23:25:26 +0800

    Choose switch ok

--- Assets/Scripts/ChooseCharactor.cs
+++ Assets/Scripts/ChooseCharactor.cs
@@ -49,36 +49,39 @@ public class ChooseCharactor : MonoBehaviour
         {
             case 1:
                 UserChoose.Id = Candy.Id;
-                UserChoose.Name.text = "Candy";
-                UserChoose.Introduction.text = "一顆頑強的糖果";
+                UserChoose.Name.text = "史帝夫‧哈靈頓";
+                UserChoose.Introduction.text = "技能效果：小糖果追蹤敵人5秒，當被追到時，小糖果會黏上去，並緩速敵人2秒。技能增強：小糖果會變快。";
                 if (!_bCreated)
                 {
-                    if (UserChoose.CharactorModel.GetChildCount() != 0)
+                    if (UserChoose.CharactorModel.childCount != 0)
                     {
                         GameObject pre = UserChoose.CharactorModel.GetChild(0).gameObject;
                         Destroy(pre);
                     }
-                    Instantiate(Models.CandyModel, UserChoose.CharactorModel.position,UserChoose.CharactorModel.rotation,UserChoose.CharactorModel);
+                    Vector3 newobjectPos = new Vector3(1.9f,-0.4f,11.4f);
+                    Debug.Log(1);
+                    Instantiate(Models.CandyModel, UserChoose.CharactorModel.position, UserChoose.CharactorModel.rotation,UserChoose.CharactorModel);
+                    UserChoose.CharactorModel.transform.localScale = new Vector3(0.3f,0.3f,0.3f);
+                    UserChoose.CharactorModel.transform.position = newobjectPos;
                     _bCreated = true;
                 }           
                 break;
             case 2:
                 UserChoose.Id = Chocolate.Id;
-                UserChoose.Name.text = "Chocolate";
-                UserChoose.Introduction.text = "一片高大的巧克力";
+                UserChoose.Name.text = "傑克‧威爾森";
+                UserChoose.Introduction.text = "技能效果：放置巧克力牆，當敵人接近時，蓋下來壓住敵人，造成敵人暈眩2秒。技能增強：巧克力牆有隱形效果。";
                 if (!_bCreated)
                 {
-                    if (UserChoose.CharactorModel.GetChildCount() != 0)
+                    if (UserChoose.CharactorModel.childCount != 0)
                     {
                         GameObject pre =UserChoose.CharactorModel.GetChild(0).gameObject;
                         Destroy(pre);
                     }
-                    Vector3 objectPos = UserChoose.CharactorModel.position;
-                    Vector3 newobjectPos = new Vector3(objectPos.x + 0.67f, objectPos.y, objectPos.z + 13.38f);
-                    Instantiate(Models.ChocolateModel, newobjectPos, UserChoose.CharactorModel.rotation, UserChoose.CharactorModel);
-                    Vector3 objectscale = UserChoose.CharactorModel.GetChild(0).gameObject.transform.localScale;
-                    UserChoose.CharactorModel.GetChild(1).gameObject.transform.localScale = new Vector3(3,3,3);
-                    Debug.Log(UserChoose.CharactorModel.GetChild(1).gameObject.name);
+                    Vector3 newobjectPos = new Vector3(-82.2f, 2.2f, 4.4f);
+                    Debug.Log(2);
+                    Instantiate(Models.ChocolateModel, UserChoose.CharactorModel.position, UserChoose.CharactorModel.rotation, UserChoose.CharactorModel);
+                    UserChoose.CharactorModel.transform.localScale = new Vector3(6,6,6);
+                    UserChoose.CharactorModel.transform.position = new Vector3(12.2f, 2.2f, 87.5f);                   
                     _bCreated = true;
                 }
                 break;
@@ -111,6 +114,7 @@ public class ChooseCharactor : MonoBehaviour
     }
     public void NextBtn()
     {
+        _bCreated = false;
         if (Input.charactor_id == 2)
         {
             Input.charactor_id = 1;
@@ -122,7 +126,8 @@ public class ChooseCharactor : MonoBehaviour
     }
     public void PreBtn()
     {
-        if(Input.charactor_id == 1)
+        _bCreated = false;
+        if (Input.charactor_id == 1)
         {
             Input.charactor_id = 2;
         }
