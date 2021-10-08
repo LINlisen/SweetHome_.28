0ca9df29a2d4a0c1d066d918e224a43627305cda
1107719008 <48972788+1107719008@users.noreply.github.com>
2021-10-08 12:32:03 +0800

    press animation

---
+++ Assets/AnimationController/PadAnimation/padPressScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class padPressScript : MonoBehaviour
+{
+    Animator animator;
+    public bool hasPressed;
+    
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        hasPressed = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasPressed)
+        {
+            animator.SetBool("hasPressed", true);
+        }
+        else
+        {
+            animator.SetBool("hasPressed", false);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            hasPressed = true;
+        }
+
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            hasPressed = false;
+        }
+    }
+}
