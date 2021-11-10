947ee701dfc842985743adf5799c620b56355d9b
1107719008 <48972788+1107719008@users.noreply.github.com>
2021-11-11 00:46:04 +0800

    change the forcefield texture

--- Assets/Scripts/Manager/GameManagement.cs
+++ Assets/Scripts/Manager/GameManagement.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using System.Linq;
+using Photon.Realtime;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 public class GameManagement : MonoBehaviour
 {
 
     [SerializeField] public GameObject setPage;
+    [SerializeField] GameObject PlayerListItemPrefab;
+    [SerializeField] Transform BlueListContent;
+    [SerializeField] Transform RedListContent;
+    Hashtable hash = new Hashtable();
     void Start()
     {
-        
+        hash.Add("WhichTeam", 0);
     }
 
     // Update is called once per frame
@@ -19,6 +26,35 @@ public class GameManagement : MonoBehaviour
             //Debug.Log("all loading ok");
         }
 
+    }
+
+    public void PlayerLeaderBoardInfoPlayerList()
+    {
+        Player[] players = PhotonNetwork.PlayerList;
+
+        foreach (Transform child in BlueListContent)
+        {
+            Destroy(child.gameObject);
+        }
+        foreach (Transform child in RedListContent)
+        {
+            Destroy(child.gameObject);
+        }
+        for (int i = 0; i < PhotonNetwork.PlayerList.Count(); i++)
+        {
+            if (i % 2 != 0)
+            {
+                Instantiate(PlayerListItemPrefab, RedListContent).GetComponent<PlayerListItem>().SetUp(players[i]);
+                hash["WhichTeam"] = 1; //紅隊為1
+                PhotonNetwork.LocalPlayer.SetCustomProperties(hash);
+            }
+            else
+            {
+                Instantiate(PlayerListItemPrefab, BlueListContent).GetComponent<PlayerListItem>().SetUp(players[i]);
+                hash["WhichTeam"] = 0; //藍隊為0
+                PhotonNetwork.LocalPlayer.SetCustomProperties(hash);
+            }
+        }
     }
     public void QuitGameSingle()
     {
@@ -37,4 +73,5 @@ public class GameManagement : MonoBehaviour
         PhotonNetwork.LeaveRoom();
         //MenuManager.Instance.OpenMenu("title");
     }
+
 }
