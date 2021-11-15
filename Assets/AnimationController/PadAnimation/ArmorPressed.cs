8d7ce431b2dc23e74113a8ca646e5560240a6cdb
LINlisen <49199663+LINlisen@users.noreply.github.com>
2021-11-16 00:06:49 +0800

    button press and up ok

---
+++ Assets/AnimationController/PadAnimation/ArmorPressed.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+public class ArmorPressed : MonoBehaviour
+{
+    Animator animator;
+    public bool hasPressed;
+    
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        hasPressed = false;
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasPressed)
+        {
+            animator.SetBool("hasPressed", true);
+        }
+        else
+        {
+            animator.SetBool("hasPressed", false);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            hasPressed = true;
+        }
+
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            hasPressed = false;
+        }
+    }
+}
--- Assets/AnimationController/PadAnimation/padPressScript.cs
+++ Assets/AnimationController/PadAnimation/padPressScript.cs
@@ -1,17 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
 public class padPressScript : MonoBehaviour
 {
     Animator animator;
     public bool hasPressed;
-    
+    [SerializeField]
+    PlayableDirector buttonTimeline;
+    [SerializeField]
+    TimelineAsset btnPressed;
+    [SerializeField]
+    TimelineAsset btnunPressed;
+    private bool _bplay;
     void Start()
     {
         animator = GetComponent<Animator>();
         hasPressed = false;
-
+        buttonTimeline = gameObject.GetComponent<PlayableDirector>();
+        _bplay = false;
+        
     }
 
     // Update is called once per frame
@@ -19,10 +28,26 @@ public class padPressScript : MonoBehaviour
     {
         if (hasPressed)
         {
+            //buttonTimeline.SetGenericBinding(animator, null);
+            buttonTimeline.playableAsset = btnunPressed;
+            if (!_bplay)
+            {
+                _bplay = true;
+                buttonTimeline.Play();
+                Debug.Log("playdown");
+            }
             animator.SetBool("hasPressed", true);
         }
         else
         {
+            //buttonTimeline.SetGenericBinding(animator, null);
+            buttonTimeline.playableAsset = btnPressed;
+            if (_bplay)
+            {
+                _bplay = false;
+                buttonTimeline.Play();
+                Debug.Log("playup");
+            }
             animator.SetBool("hasPressed", false);
         }
     }
