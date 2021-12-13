a7a7a1c4471fdb6dd3a85d136a7f2b2e2339e233
LINlisen <49199663+LINlisen@users.noreply.github.com>
2021-12-14 01:18:47 +0800

    test 6

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -350,7 +350,7 @@ public class PlayerController : MonoBehaviour
             //Move();
             if (_bWounded)
             {
-                    playerManager.animator.SetTrigger("Wounded");
+              playerManager.animator.SetTrigger("Wounded");
             }
             PlayerRotation(look.x, look.y);
             //armor
@@ -958,7 +958,7 @@ public class PlayerController : MonoBehaviour
     }
     IEnumerator WoundedSetFalseCount(string name)
     {
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(2f);
         GameObject.Find(name).GetComponent<PlayerController>()._bWounded = false;
         if (GameObject.Find(name).GetComponent<PlayerController>()._bWounded)
         {
