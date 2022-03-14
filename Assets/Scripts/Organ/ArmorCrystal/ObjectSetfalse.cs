ae8dfe656da86dc33c0ad8478afe5a9131de9d22
1107719008 <48972788+1107719008@users.noreply.github.com>
2022-03-14 22:02:17 +0800

    add

---
+++ Assets/Scripts/Organ/ArmorCrystal/ObjectSetfalse.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSetfalse : MonoBehaviour
+{
+    private bool isTouched;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isTouched = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTouched == true)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        isTouched = true;
+    }
+}
---
+++ Assets/Scripts/Organ/ArmorCrystal/SpinObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinObject : MonoBehaviour
+{
+    [SerializeField] public float RotatingSpeed = 40;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, RotatingSpeed * Time.deltaTime, 0); //rotates 50 degrees per second around z axis
+    }
+}
