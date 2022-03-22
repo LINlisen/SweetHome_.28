04fa280065de032c8165ae18f1c9436e225cadb2
1107719008 <48972788+1107719008@users.noreply.github.com>
2022-03-23 02:25:26 +0800

    armor new done

---
+++ Assets/Scripts/Organ/lookAtObj.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lookAtObj : MonoBehaviour
+{
+    public Transform target;
+    public GameObject[] playerr;
+    void Start()
+    {
+        //playerr = GameObject.FindGameObjectsWithTag("player");
+    }
+
+    void Update()
+    {
+        
+        transform.LookAt(target);
+
+    }
+}
