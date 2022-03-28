0b72aeb73e7b70c535a913da2fbc60a47baea9ba
1107719008 <48972788+1107719008@users.noreply.github.com>
2022-03-28 15:27:32 +0800

    add treasure& spin icon

---
+++ Assets/Scripts/Organ/Twotreasure.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Twotreasure : MonoBehaviour
+{
+    public bool playerOn;
+    public int playerCount;
+
+    void Start()
+    {
+        playerCount = 0;
+        playerOn = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerCount == 2)
+        {
+            Debug.Log("two persoon");
+        }
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        playerOn = true;
+        playerCount++;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        playerOn = false;
+        playerCount--;
+
+    }
+}
