3b8627430980f5192319e4c36fb7e9d6d312420d
1107719008 <48972788+1107719008@users.noreply.github.com>
2022-04-04 03:13:45 +0800

    add potion, animation not done

--- Assets/Scripts/Organ/Twotreasure.cs
+++ Assets/Scripts/Organ/Twotreasure.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.IO;
+using Photon.Pun;
 public class TwoTreasure : MonoBehaviour
 {
     public bool playerOn;
     public int playerCount;
 
-    [SerializeField]
-    public bool CanOpenTreasure;//treasure trigger flag
+    public GameObject potion1;
+    public GameObject potion2;
+
+
+    [SerializeField]public bool CanOpenTreasure;//treasure trigger flag
+
+
+   
 
     void Start()
     {
@@ -25,6 +32,11 @@ public class TwoTreasure : MonoBehaviour
         {
             Debug.Log("two persoon & treasure avalible");
             CanOpenTreasure = !CanOpenTreasure;
+        }
+        if (CanOpenTreasure)
+        {
+            potion1.gameObject.SetActive(true);
+            potion2.gameObject.SetActive(true);
         }
         
     }
