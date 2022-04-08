ab75f8b1a9b1e23bf11e16641e3b55884615bdb5
LINlisen <49199663+LINlisen@users.noreply.github.com>
2022-04-08 17:50:11 +0800

    change

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -638,7 +638,7 @@ public class PlayerController : MonoBehaviour
         if ((int)PhotonNetwork.CurrentRoom.CustomProperties["PlayerOnDuoTreasure"] == 2)
         {
             GameObject.Find("WoodChest1").GetComponent<Animator>().SetBool("openbox", true);
-            GameObject.Find("p_set").SetActive(true);
+            GameObject.Find("TwoPersonTreasure").transform.GetChild(1).gameObject.SetActive(true);
 
         }
         //armor
