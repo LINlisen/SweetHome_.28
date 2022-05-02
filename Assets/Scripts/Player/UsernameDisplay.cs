2470df9a77dea15d5e238588d122139db8c32c16
s110719001 <s110719001@stu.ntue.edu.tw>
2022-05-02 11:26:21 +0800

    playername display done

---
+++ Assets/Scripts/Player/UsernameDisplay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+
+public class UsernameDisplay : MonoBehaviour
+{
+    [SerializeField] PhotonView playerPV;
+    [SerializeField] Text text;
+
+    void Start()
+    {
+        Color blue = new Color(0, 0, 1, 1);
+        Color red = new Color(1, 0, 0, 1);
+        text.text = playerPV.Owner.NickName;
+        if(playerPV.IsMine)
+        {
+            gameObject.SetActive(false);
+        }
+        if ((int)playerPV.Owner.CustomProperties["WhichTeam"] == 0)//籃隊
+        {
+            text.color = blue;
+        }
+        else
+        {
+            text.color = red;
+        }
+    }
+}
