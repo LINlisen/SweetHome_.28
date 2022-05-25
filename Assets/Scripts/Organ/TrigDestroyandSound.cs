2c537a468a2f529a353d32ae12678f8c51fa6035
1107719008 <48972788+1107719008@users.noreply.github.com>
2022-05-26 02:58:15 +0800

    add buffed place

---
+++ Assets/Scripts/Organ/TrigDestroyandSound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrigDestroyandSound : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    public AudioSource buffsound;
+    void Start()
+    {
+        buffsound = this.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("playerbuffed");
+            buffsound.Play();
+            Destroy(collision.gameObject);
+        }
+    }
+}
