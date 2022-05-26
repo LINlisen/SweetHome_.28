54efe2cf4d0b689e8365813b5b325b2ffe37405b
1107719008 <48972788+1107719008@users.noreply.github.com>
2022-05-26 14:08:46 +0800

    add buff particle

--- Assets/Scripts/Organ/TrigDestroyandSound.cs
+++ Assets/Scripts/Organ/TrigDestroyandSound.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
 public class TrigDestroyandSound : MonoBehaviour
 {
-
     // Start is called before the first frame update
     public GameObject buffsound;
+    public GameObject Buff;
+    public GameObject getBuffParticle;
+
+
     public bool istouch;
+    [SerializeField]public int charNum;
+    public int netNum;
     void Start()
     {
         istouch = false;
+        netNum = (int)PhotonNetwork.LocalPlayer.CustomProperties["Charactor"];
     }
 
     // Update is called once per frame
@@ -18,19 +26,46 @@ public class TrigDestroyandSound : MonoBehaviour
     {
         if (istouch == true)
         {
+            //switch (switch_on)
+            //{
+            //    case 1:
+            //        Debug.Log("playerbuffed");
+            //        buffsound.GetComponent<AudioSource>().Play();
+            //        A.gameObject.SetActive(false);
+            //        break;
+            //    case 2:
+            //        Debug.Log("playerbuffed");
+            //        buffsound.GetComponent<AudioSource>().Play();
+            //        B.gameObject.SetActive(false);
+            //        break;
+            //    case 3:
+            //        Debug.Log("playerbuffed");
+            //        buffsound.GetComponent<AudioSource>().Play();
+            //        C.gameObject.SetActive(false);
+            //        break;
+            //    case 4:
+            //        Debug.Log("playerbuffed");
+            //        buffsound.GetComponent<AudioSource>().Play();
+            //        D.gameObject.SetActive(false);
+            //        break;
+
+            //}
             Debug.Log("playerbuffed");
             buffsound.GetComponent<AudioSource>().Play();
-            this.gameObject.SetActive(false);
+            Buff.gameObject.SetActive(false);
+            getBuffParticle.SetActive(true);
+            istouch = false;
         }
 
 
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player"&& netNum == charNum)
         {
             istouch = true;
         }
+        
     }
    
 }
